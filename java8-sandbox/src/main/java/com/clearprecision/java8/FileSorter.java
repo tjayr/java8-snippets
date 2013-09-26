fa2a03808b0b534f7350e3b20c0d1604b52f280a
Tony Ayres <tayres@gmail.com>
2013-09-26 14:56:27 +0100

    Adding map and foreach tests

--- java8-sandbox/src/main/java/com/clearprecision/java8/FileSorter.java
+++ java8-sandbox/src/main/java/com/clearprecision/java8/FileSorter.java
@@ -3,20 +3,33 @@ package com.clearprecision.java8;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FileSorter {
 
-	public void getData(List<String> input) {
+	public void foreach(List<String> input) {
+        System.out.println("foreach >>>>>");
 		List<String> data = new ArrayList<>();
 		data.addAll(input);
 		data.forEach(item -> System.out.println(item));
 	}
+
+    public void map(List<String> input) {
+        System.out.println("map >>>>>");
+        input.stream()
+                .filter(p -> p.contains("hello"))
+                .map(mapper -> mapper + " Jim")
+                .forEach(item -> System.out.println(item));
+
+    }
 	
 	
 	public static void main(String[] args) {
-		String[] data = {"1", "2", "3", "4"};
+		String[] data = {"hello", "2", "3", "4", "hello"};
 		FileSorter sorter = new FileSorter();
-		sorter.getData(Arrays.asList(data));
+		sorter.foreach(Arrays.asList(data));
+
+        sorter.map(Arrays.asList(data));
 	}
 
 }
