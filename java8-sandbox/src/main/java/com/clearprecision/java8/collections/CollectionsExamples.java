3f83013f5b0442b2f779c82f4bc280850bdefbf2
Tony Ayres <tayres@gmail.com>
2014-08-10 19:44:01 +0100

    Collections and streams examples

--- java8-sandbox/src/main/java/com/clearprecision/java8/collections/CollectionsExamples.java
+++ java8-sandbox/src/main/java/com/clearprecision/java8/collections/CollectionsExamples.java
@@ -1,5 +1,14 @@
 package com.clearprecision.java8.collections;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collector;
@@ -8,24 +17,55 @@ import java.util.stream.Stream;
 
 public class CollectionsExamples {
 
+    public void countOccurrencesInInFile(String fileName, String searchString) {
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+            Stream<String> textStream = Files.lines(path, Charset.defaultCharset());
+            Long total = textStream.filter(word -> word.contains("the")).collect(Collectors.counting());
+            System.out.println("Found "+total+" instances of the string \""+searchString+"\" in the file "+fileName );            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    public void filter(List<Integer> data) {
+        Stream<Integer> result = data.stream().filter(i -> i < 5);
+        result.forEach(System.out::println);
+    }
+
+    public void map(List<Integer> data) {        
+        List<Integer> newList = data.stream()
+                .map(i -> i * 10)
+                .collect(Collectors.toList());
+        System.out.println(newList);        
+    }
+
+    public void flatMap() {
+        List<List<Integer>> data = Arrays
+                .asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9));
+        
+        data.stream().flatMap( list -> list.stream() ).forEach(System.out::println);        
+
+    }
+
     public static void main(String[] args) {
-
+        CollectionsExamples ex = new CollectionsExamples();
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        System.out.println("---- Filter ----");
+        ex.filter(data);
 
-        System.out.println("ForEach");
-        data.forEach(item -> System.out.println(item));
-
-        System.out.println("Filter");
-
-        data.stream().filter(item -> item % 2 == 0).forEach(item -> System.out.println(item));
-
-        System.out.println("Map");
-
-        data.stream().map(item -> item * 2).filter(item -> item > 5).forEach(item -> System.out.println(item));
-
-        Stream<Integer> stream = data.stream().map(item -> item * 2).filter(item -> item > 5);
-        System.out.println(stream.collect(Collectors.counting()).intValue());
-
+        System.out.println("---- File Stream ----");
+        ex.countOccurrencesInInFile("stream.txt", "the");
+        
+        System.out.println("---- Mapping ----");
+        System.out.println("i -> i * 10  mapping");
+        ex.map(data);
+        
+        System.out.println("---- Flat Map ----");        
+        ex.flatMap();
     }
 
 }
